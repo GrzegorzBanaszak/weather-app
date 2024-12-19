e88a844d3020a0886687865bc4acaec15f7c7403
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2024-12-19 18:07:14 +0100

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  );
+}
+
+export default App;
